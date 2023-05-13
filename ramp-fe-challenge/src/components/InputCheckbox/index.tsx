1f377ca443cecb340e3815b2f72db2f19f1c4762
Michell <72279305+mgoris18@users.noreply.github.com>
2023-05-13 19:21:31 +0000

    Update 5 files

--- ramp-fe-challenge/src/components/InputCheckbox/index.tsx
+++ ramp-fe-challenge/src/components/InputCheckbox/index.tsx
@@ -10,17 +10,16 @@ export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, dis
       <label
         className={classNames("RampInputCheckbox--label", {
           "RampInputCheckbox--label-checked": checked,
-          "RampInputCheckbox--label-disabled": disabled,
         })}
-      />
+      >
       <input
         id={inputId}
         type="checkbox"
         className="RampInputCheckbox--input"
         checked={checked}
-        disabled={disabled}
         onChange={() => onChange(!checked)}
       />
+      </label>
     </div>
   )
 }
